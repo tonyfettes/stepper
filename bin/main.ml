3d22484fda665f5617702d743a7af006412c5d96
Tony Fettes <tonyfettes@tonyfettes.com>
2024-02-13 20:25:53 +0800

    fix priority

--- bin/main.ml
+++ bin/main.ml
@@ -51,6 +51,7 @@ end = struct
 
   let rec matches (pat : t) (exp : Exp.t) =
     match (pat, exp) with
+    | pat, Residue (_, _, _, exp) -> matches pat exp
     | Any, _ -> true
     | Val, Int _ | Val, Fun _ -> true
     | Val, _ -> false
@@ -73,7 +74,7 @@ and Exp : sig
     | Fun of string * t
     | Fix of string * t
     | Filter of Pat.t * Act.t * Gas.t * t
-    | Residue of Act.t * Gas.t * t
+    | Residue of Act.t * Gas.t * int * t
 
   val to_string : t -> string
   val eq : t -> t -> bool
@@ -87,7 +88,7 @@ end = struct
     | Fun of string * t
     | Fix of string * t
     | Filter of Pat.t * Act.t * Gas.t * t
-    | Residue of Act.t * Gas.t * t
+    | Residue of Act.t * Gas.t * int * t
 
   let rec to_string = function
     | Var var -> var
@@ -100,9 +101,9 @@ end = struct
     | Filter (p, a, g, e) ->
         Printf.sprintf "(filter %s do %s for %s in %s)" (Pat.to_string p)
           (Act.to_string a) (Gas.to_string g) (to_string e)
-    | Residue (a, g, e) ->
-        Printf.sprintf "(do %s for %s in %s)" (Act.to_string a)
-          (Gas.to_string g) (to_string e)
+    | Residue (a, g, l, e) ->
+        Printf.sprintf "(do %s for %s at %d in %s)" (Act.to_string a)
+          (Gas.to_string g) l (to_string e)
 
   let rec eq (this : t) (that : t) =
     match (this, that) with
@@ -122,8 +123,8 @@ end = struct
     | Fix (this_x, this_e), Fix (that_x, that_e) ->
         this_x == that_x && eq this_e that_e
     | Fix _, _ -> false
-    | ( (Filter (_, _, _, this) | Residue (_, _, this)),
-        (Filter (_, _, _, that) | Residue (_, _, that)) ) ->
+    | ( (Filter (_, _, _, this) | Residue (_, _, _, this)),
+        (Filter (_, _, _, that) | Residue (_, _, _, that)) ) ->
         eq this that
     | (Filter _ | Residue _), _ -> false
 
@@ -143,7 +144,7 @@ end = struct
         if fun_var == var then Fun (fun_var, body)
         else Fun (fun_var, subst body var expr)
     | Filter (p, a, g, e) -> Filter (p, a, g, subst e var expr)
-    | Residue (a, g, e) -> Residue (a, g, subst e var expr)
+    | Residue (a, g, l, e) -> Residue (a, g, l, subst e var expr)
 end
 
 and Val : sig
@@ -210,11 +211,11 @@ module Result = struct
         | `Err (err, exp) -> `Err (err, exp)
         | `Val value -> `Exp value
         | `Exp exp -> `Exp (Filter (p, a, g, exp)))
-    | Residue (a, g, e) -> (
+    | Residue (a, g, l, e) -> (
         match transition e with
         | `Err (err, exp) -> `Err (err, exp)
         | `Val exp -> `Exp exp
-        | `Exp exp -> `Exp (Residue (a, g, exp)))
+        | `Exp exp -> `Exp (Residue (a, g, l, exp)))
 
   let rec eval (expr : Exp.t) =
     match expr with
@@ -233,7 +234,7 @@ module Result = struct
     | Fun (x, e) -> `Val (Fun (x, e))
     | Fix (x, e) -> `Val (Fun (x, Exp.subst e x e))
     | Filter (_, _, _, e) -> eval e
-    | Residue (_, _, e) -> eval e
+    | Residue (_, _, _, e) -> eval e
 end
 
 module Ctx = struct
@@ -244,7 +245,7 @@ module Ctx = struct
     | App_l of t * Exp.t
     | App_r of Exp.t * t
     | Filter of Pat.t * Act.t * Gas.t * t
-    | Residue of Act.t * Gas.t * t
+    | Residue of Act.t * Gas.t * int * t
 
   let rec to_string (ctx : t) =
     match ctx with
@@ -256,8 +257,8 @@ module Ctx = struct
     | Filter (p, a, g, c) ->
         Printf.sprintf "filter %s do %s for %s in %s" (Pat.to_string p)
           (Act.to_string a) (Gas.to_string g) (to_string c)
-    | Residue (a, g, c) ->
-        Printf.sprintf "do %s for %s in %s" (Act.to_string a) (Gas.to_string g)
+    | Residue (a, g, l, c) ->
+        Printf.sprintf "do %s for %s at %d in %s" (Act.to_string a) (Gas.to_string g) l
           (to_string c)
 
   let rec decompose (exp : Exp.t) =
@@ -283,8 +284,8 @@ module Ctx = struct
     | Fix (x, d) -> (Top, Exp.Fix (x, d)) :: []
     | Filter (p, a, g, e) ->
         decompose e |> List.map (fun (c, e) -> (Filter (p, a, g, c), e))
-    | Residue (a, g, e) ->
-        decompose e |> List.map (fun (c, e) -> (Residue (a, g, c), e))
+    | Residue (a, g, l, e) ->
+        decompose e |> List.map (fun (c, e) -> (Residue (a, g, l, c), e))
 
   let rec compose (ctx : t) (exp : Exp.t) =
     match ctx with
@@ -294,12 +295,12 @@ module Ctx = struct
     | App_l (c_l, e_r) -> App (compose c_l exp, e_r)
     | App_r (e_l, c_r) -> App (e_l, compose c_r exp)
     | Filter (p, a, g, c) -> Filter (p, a, g, compose c exp)
-    | Residue (a, g, c) -> Residue (a, g, compose c exp)
+    | Residue (a, g, l, c) -> Residue (a, g, l, compose c exp)
 end
 
-let rec instr (pat : Pat.t) (act : Act.t) (gas : Gas.t) (exp : Exp.t) =
+let rec instr (pat : Pat.t) (act : Act.t) (gas : Gas.t) (lvl : int) (exp : Exp.t) =
   let wrap exp =
-    if Pat.matches pat exp then Exp.Residue (act, gas, exp) else exp
+    if Pat.matches pat exp then Exp.Residue (act, gas, lvl, exp) else exp
   in
   match Result.transition exp with
   | `Err _ -> exp
@@ -309,59 +310,66 @@ let rec instr (pat : Pat.t) (act : Act.t) (gas : Gas.t) (exp : Exp.t) =
       | Var var -> wrap (Var var)
       | Int int -> Int int
       | Add (e_l, e_r) ->
-          let e_l = instr pat act gas e_l in
-          let e_r = instr pat act gas e_r in
+          let e_l = instr pat act gas lvl e_l in
+          let e_r = instr pat act gas lvl e_r in
           wrap (Add (e_l, e_r))
       | App (e_l, e_r) ->
-          let e_l = instr pat act gas e_l in
-          let e_r = instr pat act gas e_r in
+          let e_l = instr pat act gas lvl e_l in
+          let e_r = instr pat act gas lvl e_r in
           wrap (App (e_l, e_r))
       | Fun (x, e) -> Fun (x, e)
       | Fix (x, e) -> wrap (Fix (x, e))
       | Filter (p, a, g, e) ->
-          let e = instr pat act gas e in
-          Filter (p, a, g, instr p a g e)
-      | Residue (a, g, e) ->
-          let e = instr pat act gas e in
-          Residue (a, g, e))
+          let e = instr pat act gas lvl e in
+          Filter (p, a, g, instr p a g (lvl + 1) e)
+      | Residue (a, g, l, e) ->
+          let e = instr pat act gas lvl e in
+          Residue (a, g, l, e))
 
-let rec annot (act : Act.t) (gas : Gas.t) (ctx : Ctx.t) =
+let rec annot (act : Act.t) (gas : Gas.t) (lvl : int) (ctx : Ctx.t) =
   match ctx with
   | Top -> (act, gas, ctx)
   | Add_l (c, e) ->
-    let (act, gas, c) = annot act gas c in
+    let (act, gas, c) = annot act gas lvl c in
     (act, gas, Add_l (c, e))
   | Add_r (e, c) -> 
-    let (act, gas, c) = annot act gas c in
+    let (act, gas, c) = annot act gas lvl c in
     (act, gas, Add_r (e, c))
   | App_l (c, e) ->
-    let (act, gas, c) = annot act gas c in
+    let (act, gas, c) = annot act gas lvl c in
     (act, gas, App_l (c, e))
   | App_r (e, c) ->
-    let (act, gas, c) = annot act gas c in
+    let (act, gas, c) = annot act gas lvl c in
     (act, gas, App_r (e, c))
   | Filter (p, a, g, c) ->
-    let (act, gas, c) = annot act gas c in
+    let (act, gas, c) = annot act gas lvl c in
     (act, gas, Filter (p, a, g, c))
-  | Residue (a, One, c) -> annot a One c
-  | Residue (a, All, c) ->
-    let (act, gas, c) = annot act gas c in
-    (act, gas, Residue (a, All, c))
+  | Residue (a, One, l, c) ->
+    if l > lvl then
+      annot a One l c
+    else
+      annot act One lvl c
+  | Residue (a, All, l, c) ->
+    if l > lvl then
+      let (act, gas, c) = annot a All l c in
+      (act, gas, Residue (a, All, l, c))
+    else
+      annot act gas lvl c
 
 let rec step (exp : Exp.t) =
   Printf.printf "stepping %s\n" (Exp.to_string exp);
-  let instr'd = instr Any Pause One exp in
+  let instr'd = instr Any Pause One 0 exp in
   Printf.printf "instruct %s\n" (Exp.to_string instr'd);
   let decomposed = Ctx.decompose instr'd in
   Printf.printf "decomposed\n";
   decomposed |>
   List.iteri (fun i (c, e) -> Printf.printf "%2d: %s { %s }\n" i (Ctx.to_string c) (Exp.to_string e));
   let annot'd = decomposed |> List.map (fun (ctx, exp) ->
-    let (act, gas, ctx) = annot Pause One ctx in 
+    let (act, gas, ctx) = annot Pause One 0 ctx in 
     (act, gas, ctx, exp)) in
   Printf.printf "annot'd\n";
   annot'd |>
-    List.iteri (fun i (_, _, c, e) -> Printf.printf "%2d: %s { %s }\n" i (Ctx.to_string c) (Exp.to_string e));
+    List.iteri (fun i (a, _, c, e) -> Printf.printf "%2d: %s: %s { %s }\n" i (Act.to_string a) (Ctx.to_string c) (Exp.to_string e));
   match List.find_opt (fun (act, gas, ctx, exp) -> act == Act.Skip) annot'd with
   | None -> annot'd
   | Some (_, _, ctx, exp) ->
@@ -393,18 +401,18 @@ let () =
 let () =
   let body = Exp.Add (Add (Int 1, Int 2), Add (Int 3, Int 4)) in
   let exp = Exp.Filter (Pat.(Add (Val, Val)), Act.Pause, One, body) in
-  let instr'ed = instr Any Skip All exp in
+  let instr'ed = instr Any Skip All 0 exp in
   instr'ed |> Exp.to_string |> print_endline;
   let objs = instr'ed |> Ctx.decompose in
   objs |> List.iter (fun (c, e) ->
     Printf.printf "%s { %s }\n" (Ctx.to_string c) (Exp.to_string e));
   objs |> List.iter (fun (c, e) ->
-    let (act, gas, c) = annot Skip All c in
+    let (act, gas, c) = annot Skip All 0 c in
     Printf.printf "act = %s, gas = %s, %s { %s }\n" (Act.to_string act) (Gas.to_string gas) (Ctx.to_string c) (Exp.to_string e))
 
 let () =
-  let body = Exp.Add (Add (Int 1, Int 2), Add (Int 1, Int 2)) in
-  let exp = Exp.Filter (Pat.(Add ((Int 1), (Int 2))), Act.Skip, One, body) in
+  let body = Exp.Add (Add (Add (Int 1, Int 2), Int 3), Add (Add (Int 1, Int 2), Int 3)) in
+  let exp = Exp.Filter (Pat.Add (Add (Int 1, Int 2), Int 3), Act.Skip, All, body) in
   let trunk = step exp |> List.map (fun (_, _, c, e) ->
     Printf.sprintf "%s { %s }" (Ctx.to_string c) (Exp.to_string e)) |> String.concat "\n" in
   Printf.printf "========\n%s\n---------\n" trunk
