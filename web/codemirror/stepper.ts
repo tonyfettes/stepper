f6c9d6c3c5990e38465175f832ad082515f54d7d
Tony Fettes <tonyfettes@tonyfettes.com>
2024-05-27 09:19:17 +0800

    feat: add more grammar to codemirror

--- web/codemirror/stepper.ts
+++ web/codemirror/stepper.ts
@@ -7,11 +7,19 @@ export const stepperLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       styleTags({
-        keyword: t.keyword
+        "let rec in fun": t.definitionKeyword,
+        "if then else eval hide pause debug filter": t.controlKeyword,
+        Identifier: t.variableName,
+        Comment: t.blockComment,
+        ArithOp: t.arithmeticOperator,
+        Integer: t.integer,
+        Boolean: t.bool,
+        "( )": t.paren,
       })
     ]
   }),
   languageData: {
+    commentTokens: { block: { open: "#", close: "#" } },
   }
 });
 
